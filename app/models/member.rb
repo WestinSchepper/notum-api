602ecf8b42ea0b91f47914c0d9a854459a72de0f
Westin Schepper <westin@teeps.org>
2016-09-30 21:32:10 -0400

    Added annotation to models

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: members
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  email      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Member < ApplicationRecord
   has_and_belongs_to_many :projects
 end
