a97e8c6dcf0e0f85069679c4d2be3a683305c730
Westin Schepper <westin@teeps.org>
2016-09-30 21:27:14 -0400

    Adds unique index to project name column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160930232313) do
+ActiveRecord::Schema.define(version: 20161001012531) do
 
   create_table "members", force: :cascade do |t|
     t.string   "name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20160930232313) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_projects_on_name", unique: true
   end
 
 end
