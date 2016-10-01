1d0866c1242fefddaac7d64f5a37f743a253538c
Westin Schepper <westin@teeps.org>
2016-10-01 17:35:10 -0400

    Includes :standups in project response

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -10,7 +10,7 @@ class ProjectsController < ApplicationController
 
   # GET /projects/1
   def show
-    render json: @project.to_json(:include => [:members])
+    render json: @project.to_json(:include => [:members, :standups])
   end
 
   # POST /projects
