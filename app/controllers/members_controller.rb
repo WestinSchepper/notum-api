78e3886ebe729bf1a65aa52ed28087dec04c1367
Westin Schepper <westin@teeps.org>
2016-10-01 10:59:23 -0400

    Adds projects to response when viewing one member

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -10,7 +10,7 @@ class MembersController < ApplicationController
 
   # GET /members/1
   def show
-    render json: @member
+    render json: @member.to_json(:include => [:projects])
   end
 
   # POST /members
