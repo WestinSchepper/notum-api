4d67ba3bb74c4b262ae89d9396b9f687ca18f5e0
Westin Schepper <westin@teeps.org>
2016-10-01 17:15:34 -0400

    Added annotation to Standups model

--- test/models/standup_test.rb
+++ test/models/standup_test.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: standups
+#
+#  id          :integer          not null, primary key
+#  did         :text
+#  doing       :text
+#  impediments :text
+#  project_id  :integer
+#  member_id   :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'test_helper'
 
 class StandupTest < ActiveSupport::TestCase
