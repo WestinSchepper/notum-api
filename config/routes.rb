9e17443572a5009e3ac9c768d14f6e97b6894079
Westin Schepper <westin@teeps.org>
2016-09-30 21:24:35 -0400

    Replaces :delete with :destroy in in member routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :members, only: [:index, :create, :show, :update, :delete]
+  resources :members, only: [:index, :create, :show, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
